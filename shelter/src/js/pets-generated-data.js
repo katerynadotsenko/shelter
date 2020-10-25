c8757bc7ae8fea8e6f00c7631f983c49c5d7911c
Kateryna Dotsenko <ekatrinadotsenko@gmail.com>
2020-10-25 11:50:35 +0100

    fix: add proper ^Cts generation for page with 6 items

--- shelter/src/js/pets-generated-data.js
+++ shelter/src/js/pets-generated-data.js
@@ -5,23 +5,32 @@ export default class PetsGeneratedData {
         this.data = data;
     }
 
-    shuffle() {
+    shuffle(array) {
         let j, temp;
-        for (var i = this.data.length - 1; i > 0; i--){
+        for (let i = array.length - 1; i > 0; i--){
             j = Math.floor(Math.random()*(i + 1));
-            temp = this.data[j];
-            this.data[j] = this.data[i];
-            this.data[i] = temp;
+            temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
         }
+        return array;
+    }
+
+    shuffleForPage6(i) {
+        const firstPart = this.shuffle(this.data.slice(0, this.data.length - i * 2));
+        const secondPart = this.shuffle(this.data.slice(this.data.length - i * 2, this.data.length));
+        //console.log("firstPart - ", firstPart);
+        //console.log("secondPart - ", secondPart);
+        this.data = [...firstPart, ...secondPart];
         return this.data;
     }
 
     generatePetsData() {
         let generatePetsArray = [];
-        for (let i = 1; i <= 6; i++) {
-            generatePetsArray.push(...this.shuffle());
+        for (let i = 0; i <= 5; i++) {
+            generatePetsArray.push(...this.shuffleForPage6(i));
         }
-        console.log("generatePetsArray - ", generatePetsArray);
+        //console.log("generatePetsArray - ", generatePetsArray);
         return generatePetsArray;
     }
 }
