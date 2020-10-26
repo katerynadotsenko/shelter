9df278c2af91bad5cbb81a47313255b2c7ba99bd
Kateryna Dotsenko <ekatrinadotsenko@gmail.com>
2020-10-26 12:45:37 +0100

    fix: add sticky header right gap, when modal is opened

--- shelter/src/js/components/modal.js
+++ shelter/src/js/components/modal.js
@@ -30,7 +30,11 @@ export default class Modal {
 
         if (this.bodyWidth < document.body.clientWidth) {
             const scrollWidth = document.body.clientWidth - this.bodyWidth;
+            const headerSticky = document.querySelector('.header_sticky');
             document.body.style.paddingRight = `${scrollWidth}px`;
+            if (headerSticky) {
+                headerSticky.style.paddingRight = `${scrollWidth}px`;
+            }
         }
 
         const overlay = document.querySelector('.overlay');
@@ -60,12 +64,16 @@ export default class Modal {
         if (classes.contains('overlay') || classes.contains('modal__close-button')) {
             const overlay = document.querySelector('.overlay');
             const modal = document.querySelector('.modal');
+            const headerSticky = document.querySelector('.header_sticky');
             overlay.style.opacity = '0';
             modal.style.opacity = '0';
             setTimeout(() => {
                 document.querySelector('.overlay').remove();
                 document.body.classList.toggle('noscroll');
                 document.body.style.paddingRight = '0';
+                if (headerSticky) {
+                    headerSticky.style.paddingRight = '0';
+                }
             }, 300);
         }
     }
